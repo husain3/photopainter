80577fac7ae1578fccb053f0b145735a124baa10
Arshad Husain <arshad.husain1@gmail.com>
2022-06-06 21:21:54 -0600

    Remove unnecessary js functions

--- src/index.js
+++ src/index.js
@@ -48,37 +48,6 @@ const drawOutputImage = (imageData, canvasId) => {
   outputImageOverlay.height = imageData.height
 }
 
-const drawBoundingBox = (boundingBox, canvasId) => {
-  console.log('[drawBoundingBox]')
-  const canvas = document.getElementById(canvasId)
-  const ctx = canvas.getContext('2d')
-  ctx.strokeStyle = 'blue'
-  ctx.lineWidth = 1
-  ctx.strokeRect(...inset(...boundingBox, 2, 2))
-}
-
-const drawPoints = (points, canvasId, colour) => {
-  console.log('[drawPoints]')
-  const canvas = document.getElementById(canvasId)
-  const ctx = canvas.getContext('2d')
-  ctx.strokeStyle = colour
-  ctx.lineWidth = 1
-  const path = new Path2D()
-  points.forEach(({ x, y }, index) => index === 0 ? path.moveTo(x, y) : path.lineTo(x, y))
-  path.closePath()
-  ctx.stroke(path)
-}
-
-const drawCorners = (corners, canvasId) => {
-  console.log('[drawCorners]')
-  drawPoints(corners, canvasId, 'magenta')
-}
-
-export const drawContour = (points, canvasId) => {
-  console.log('[drawContour]')
-  drawPoints(points, canvasId, 'red')
-}
-
 const cropCells = (canvasId, cellsId, boundingBox) => {
   console.log('[cropCells]')
 
@@ -151,78 +120,12 @@ const loadInputImage = async index => {
   reset()
 }
 
-const onSelectImageSudoku = e => {
-  console.log('[onSelectImageSudoku]')
-  loadInputImage(e.target.selectedIndex)
-}
-
 const unpackImage = (module, [width, height, channels, addr]) => {
   const cb = width * height * channels
   const data = module.HEAPU8.slice(addr, addr + cb)
   return channels === 1
     ? imageDataFrom1Channel(data, width, height)
     : imageDataFrom4Channels(data, width, height)
-}
-
-const unpackCorners = data32 => {
-  return R.splitEvery(2, data32).map(([x, y]) => ({ x, y }))
-}
-
-const unpackContour = (module, [numPoints, addr]) => {
-  const addr32 = addr / module.HEAP32.BYTES_PER_ELEMENT
-  const data32 = module.HEAP32.slice(addr32, addr32 + numPoints * 2)
-  return R.splitEvery(2, data32).map(([x, y]) => ({ x, y }))
-}
-
-const unpackProcessImageResult = (module, addr) => {
-  const NUM_INT_FIELDS = 22
-  const addr32 = addr / module.HEAP32.BYTES_PER_ELEMENT
-  const data32 = module.HEAP32.slice(addr32, addr32 + NUM_INT_FIELDS)
-  const boundingBox = data32.slice(0, 4)
-  const image1 = unpackImage(module, data32.slice(4, 8))
-  const image2 = unpackImage(module, data32.slice(8, 12))
-  const corners = unpackCorners(data32.slice(12, 20))
-  const contour = unpackContour(module, data32.slice(20, 22))
-  return { boundingBox, image1, image2, corners, contour }
-}
-
-const onProcessImage = (module, processImage) => () => {
-  console.log('[onProcessImage]')
-  reset()
-  const { data, width, height } = getImageData()
-
-  const startTime = performance.now()
-  const addr = processImage(data, width, height)
-  const endTime = performance.now()
-
-  const elapsedTimeRow = document.getElementById('elapsed-time-row')
-  elapsedTimeRow.style.display = 'block';
-  const elapsedTime = document.getElementById('elapsed-time')
-  elapsedTime.innerText = (endTime - startTime).toFixed(2)
-
-  const unpackedResult = unpackProcessImageResult(module, addr)
-  module._free(addr)
-
-  drawOutputImage(unpackedResult.image1, 'output-image-1')
-  drawOutputImage(unpackedResult.image2, 'output-image-2')
-  drawBoundingBox(unpackedResult.boundingBox, 'output-image-1-overlay')
-  drawCorners(unpackedResult.corners, 'output-image-1-overlay')
-  drawContour(unpackedResult.contour, 'output-image-1-overlay')
-
-  const boundingBox1 = unpackedResult.boundingBox
-  const boundingBox2 = [0, 0, unpackedResult.image2.width, unpackedResult.image2.height]
-
-  cropCells('output-image-1', 'cells-1', boundingBox1)
-  cropCells('output-image-2', 'cells-2', boundingBox2)
-}
-
-const wrapProcessImage = module => {
-  console.log('[wrapProcessImage]')
-  const ident = 'processImage'
-  const returnType = 'number'
-  const argTypes = ['array', 'number', 'number']
-  const processImage = module.cwrap(ident, returnType, argTypes)
-  return processImage
 }
 
 const init = module => {
@@ -237,11 +140,7 @@ const init = module => {
     optionElement.innerText = label
     inputImageSelector.appendChild(optionElement)
   })
-  inputImageSelector.addEventListener('change', onSelectImageSudoku)
   loadInputImage(0)
-  const processImage = wrapProcessImage(module)
-  const processImageBtn = document.getElementById('process-image-btn')
-  processImageBtn.addEventListener('click', onProcessImage(module, processImage))
 }
 
 const main = async () => {
